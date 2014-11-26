8bab038e22bb3a1c02ccfe9736d8d9db8f41f732
Roth Michaels <roth@rothmichaels.us>
2014-11-26 04:41:07 -0500

    New compojure project.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 psl-upload
 ==========
 
-A clojure file uploader and file server
+A Clojure HTTP server application that provides a simple file upload REST API.
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2014 Roth Michaels.
+
+Licensed under the [Eclipse Public License - v1.0](http://www.eclipse.org/legal/epl-v10.html).
---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject psl-upload "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [hiccup "1.0.5"]
+                 [markdown-clj "0.9.58" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler psl-upload.core.handler/app
+         :nrepl {start? true :port 5555}}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
